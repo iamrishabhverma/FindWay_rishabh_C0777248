5e9105756b84f478d33912843d0d2eb736d25940
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2020-06-10 10:12:10 -0400

    Initial Commit

---
+++ FindWay_rishabh_C0777248/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindWay_rishabh_C0777248
+//
+//  Created by Rishabh Verma on 2020-06-10.
+//  Copyright © 2020 Rishabh Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FindWay_rishabh_C0777248Tests/FindWay_rishabh_C0777248Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FindWay_rishabh_C0777248Tests.swift
+//  FindWay_rishabh_C0777248Tests
+//
+//  Created by Rishabh Verma on 2020-06-10.
+//  Copyright © 2020 Rishabh Verma. All rights reserved.
+//
+
+import XCTest
+@testable import FindWay_rishabh_C0777248
+
+class FindWay_rishabh_C0777248Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
