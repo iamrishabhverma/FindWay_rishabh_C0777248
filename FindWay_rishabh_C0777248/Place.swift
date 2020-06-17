65f8decb16479a45816bd0af888326d12a43cf9e
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2020-06-17 13:13:01 -0400

    h

---
+++ FindWay_rishabh_C0777248/Place.swift
@@ -0,0 +1,40 @@
+//
+//  Place.swift
+//  FindWay_rishabh_C0777248
+//
+//  Created by Rishabh Verma on 2020-06-17.
+//  Copyright © 2020 Rishabh Verma. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Place: NSObject, MKAnnotation {
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+    
+    static func getPlaces() -> [Place] {
+        guard let path = Bundle.main.path(forResource: "Places", ofType: "plist"), let array = NSArray(contentsOfFile: path) else { return [] }
+        
+        var places = [Place]()
+        
+        for item in array {
+            let dictionary = item as? [String : Any]
+            let title = dictionary?["title"] as? String
+            let subtitle = dictionary?["description"] as? String
+            let latitude = dictionary?["latitude"] as? Double ?? 0, longitude = dictionary?["longitude"] as? Double ?? 0
+            
+            let place = Place(title: title, subtitle: subtitle, coordinate: CLLocationCoordinate2DMake(latitude, longitude))
+            places.append(place)
+        }
+        
+        return places as [Place]
+    }
+}
